bb65ed333f01f258d118375c5b9b74ad338833a6
Biricz Bence <71565433+BenceBiricz@users.noreply.github.com>
2024-03-20 19:57:26 +0100

    Update
    - Created the basic structure for the webpage

--- src/app/pages/main-page/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page/main-page.component.ts
@@ -1,10 +1,112 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+// import data from '../../assets/data/card-data.json';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrl: './main-page.component.scss'
+  styleUrl: './main-page.component.scss',
 })
-export class MainPageComponent {
+export class MainPageComponent implements OnInit {
+  ngOnInit() {
+    // console.log(data);
+  }
 
+  columns = [
+    {
+      columnItem: [
+        {
+          title: 'Basic',
+          items: ['1', '2', '3'],
+        },
+        {
+          title: 'Basic',
+          items: ['1', '2', '3', '3', '4', '1', '2', '3', '4'],
+        },
+        {
+          title: 'Basic',
+          items: ['1', '2', '3'],
+        },
+        {
+          title: 'Basic',
+          items: ['1', '2', '3', '4'],
+        },
+        {
+          title: 'Basic',
+          items: ['1', '2', '3', '4', '1', '2', '3', '4'],
+        },
+      ],
+    },
+    {
+      columnItem: [
+        {
+          title: 'Basic',
+          items: ['1', '2', '3', '4', '1', '2'],
+        },
+        {
+          title: 'Basic',
+          items: ['1', '2', '4', '1', '2', '3', '4'],
+        },
+        {
+          title: 'Basic',
+          items: ['1', '2', '3'],
+        },
+        {
+          title: 'Basic',
+          items: ['1', '2', '3', '4'],
+        },
+        {
+          title: 'Basic',
+          items: ['1', '1', '2', '3', '4'],
+        },
+      ],
+    },
+    {
+      columnItem: [
+        {
+          title: 'Basic',
+          items: ['1', '2', '3', '4', '1', '2', '3', '4', '1', '2', '3', '4'],
+        },
+        {
+          title: 'Basic',
+          items: ['1', '2', '3'],
+        },
+        {
+          title: 'Basic',
+          items: ['1', '2', '3'],
+        },
+        {
+          title: 'Basic',
+          items: ['1', '2', '3', '4'],
+        },
+        {
+          title: 'Basic',
+          items: ['1', '2', '3', '4', '1', '2', '3', '4'],
+        },
+      ],
+    },
+    {
+      columnItem: [
+        {
+          title: 'Basic',
+          items: ['1', '2', '3'],
+        },
+        {
+          title: 'Basic',
+          items: ['1', '2', '3', '4', '1', '2', '3', '4', '1', '2', '3', '4'],
+        },
+        {
+          title: 'Basic',
+          items: ['1', '2', '3'],
+        },
+        {
+          title: 'Basic',
+          items: ['1', '2', '3', '4'],
+        },
+        {
+          title: 'Basic',
+          items: ['1', '2', '3', '4', '1', '2', '3', '4'],
+        },
+      ],
+    },
+  ];
 }
