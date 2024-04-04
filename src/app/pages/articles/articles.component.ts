4530475713077bb5d111ccb617456be3e5c4afeb
Biricz Bence <71565433+BenceBiricz@users.noreply.github.com>
2024-04-04 15:49:01 +0200

    Adding blogs dinamically

--- src/app/pages/articles/articles.component.ts
+++ src/app/pages/articles/articles.component.ts
@@ -1,8 +1,37 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-articles',
   templateUrl: './articles.component.html',
   styleUrl: './articles.component.scss',
 })
-export class ArticlesComponent {}
+export class ArticlesComponent implements OnInit {
+  dataPath = 'power_of_cheat_sheets';
+  data: any;
+  loaded = false;
+
+  constructor(private http: HttpClient, private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    //this.dataPath
+    this.route.queryParams.subscribe((params) => {
+      this.dataPath = params['article'];
+      console.log(params['article']);
+    });
+
+    this.http
+      .get('assets/articles/' + this.dataPath + '.json')
+      .subscribe((data: any) => {
+        if (data) {
+          this.data = data!;
+          this.loaded = true;
+        }
+      });
+  }
+
+  back() {
+    history.back();
+  }
+}
--- src/app/pages/blogs/blogs.component.ts
+++ src/app/pages/blogs/blogs.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-blogs',
   templateUrl: './blogs.component.html',
-  styleUrl: './blogs.component.scss'
+  styleUrl: './blogs.component.scss',
 })
 export class BlogsComponent {
+  constructor(private router: Router) {}
 
+  blogs = [
+    {
+      title: 'The Power of Cheat Sheets: A Shortcut to Learning',
+      text: 'Are you looking for a quick and effective way to learn new things? Look no further than cheat sheets.',
+      img: 'coding.png',
+      data: 'power_of_cheat_sheets',
+    },
+    {
+      title: 'A Quick Guide to Web Development Learning Path',
+      text: `Embarking on a journey to become a proficient web developer requires a structured approach. Here's a concise roadmap to guide you through the essential concepts.`,
+      img: 'monitors.jpg',
+      data: 'web_dev_guide',
+    },
+  ];
+
+  openArticle(file: string) {
+    this.router.navigate(['articles'], { queryParams: { article: file } });
+  }
 }
